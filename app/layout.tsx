0066ab8d7bab53b0b322d17cf2c8967803eb5700
aortiz13 <serviciosmensualesadrian@gmail.com>
2026-01-21 17:23:54 -0300

    Refactor: Enhanced Widget UI with Dialog, Animations and Sonner

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Outfit, Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
+import { Toaster } from "@/components/ui/sonner";
 
 const outfit = Outfit({
   variable: "--font-heading",
